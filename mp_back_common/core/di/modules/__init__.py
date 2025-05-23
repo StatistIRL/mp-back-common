8b8173f68122b145194820931ec6082aadea8631
StatistIRL <vasry7@gmail.com>
2025-05-23 08:35:56 +0300

    df

--- mp_back_common/core/di/modules/__init__.py
+++ mp_back_common/core/di/modules/__init__.py
@@ -1,10 +1,12 @@
 from .connections import _redis, _elasticsearch, database, api_client_session
 from .mp_clients import topics_service
+from . import authentication
 
 __all__ = [
     "_redis",
     "_elasticsearch",
     "database",
     "api_client_session",
-    "topics_service"
+    "topics_service",
+    "authentication"
 ]
