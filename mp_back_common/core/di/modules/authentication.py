efe942253341631fecb663dcd9ee60735a10ced7
StatistIRL <vasry7@gmail.com>
2025-05-23 08:28:14 +0300

    df

---
+++ mp_back_common/core/di/modules/authentication.py
@@ -0,0 +1,8 @@
+import aioinject
+from .._types import Providers
+from ...middlewares.authentication import ClientIdContextVar
+from ...middlewares.dto import UserCredentials
+
+PROVIDERS: Providers = [
+    aioinject.Scoped(lambda: ClientIdContextVar.get(), UserCredentials)
+]
